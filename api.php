5d5e42c5dc82353059fda15071f891c967af5305
GamalielWP <56947950+GamalielWP@users.noreply.github.com>
2020-12-28 16:09:58 +0700

    Tugas_API

---
+++ api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('filmanime','apianimecontroller@get_all_anime');
+Route::post('tambahfilmanime','apianimecontroller@insert_data_anime');
+Route::put('updatefilmanime/{kode_anime}','apianimecontroller@update_data_anime');
+Route::delete('deletefilmanime/{kode_anime}','apianimecontroller@delete_data_anime');
